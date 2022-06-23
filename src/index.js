5dcb4bdb3cff53156aff571feee3e7e6d10fa050
ihamza494 <ihamza494@gmail.com>
2022-06-23 12:02:58 +0500

    React useState

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import HeaderComponent from "./HeaderComponent";
-import DropdownBox from "./Dropdown";
+import Practice from "./Practice";
+// import HeaderComponent from "./HeaderComponent";
+// import DropdownBox from "./Dropdown";
 // import Netforce from "./Netforce";
 // import Speeddiff from "./Speeddiff";
 // import Distancetrvl from "./Distance_trvl";
@@ -9,8 +10,9 @@ import DropdownBox from "./Dropdown";
 function App() {
   return (
     <>
-      <HeaderComponent />
-      <DropdownBox />
+      <Practice />
+      {/* <HeaderComponent />
+      <DropdownBox /> */}
       {/* <Netforce />
       <Speeddiff />
       <Distancetrvl /> */}
